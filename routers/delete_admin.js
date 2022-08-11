1017607c40b5267b42331417a1272ae71e6ec83c
justman-1 <sharom573@gmail.com>
2022-08-11 16:29:36 +0300

    1

--- routers/delete_admin.js
+++ routers/delete_admin.js
@@ -7,7 +7,12 @@ const jwt = require('jsonwebtoken')
 async function addAdmin(req, res){
     const id = req.body.id
     if(req.login != 'admin') return res.status(410).send('У вас недостаточно прав.')
+    find
     User.deleteOne({_id: id}, (err, result)=>{
+        if(result){
+            let cacheToken = cache.get('token:' + result.login)
+            if(cacheToken) cache.del('token:' + result.login)
+        }
         if(err){
             console.log(err)
             return res.status(410).send('Ошибка сервера')
